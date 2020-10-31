cf5d2ccdc717274f2a9558b91ec986062bbffec5
gvemulapalli33 <gvemulapalli33@gmail.com>
2020-10-31 09:31:58 -0700

    poster

---
+++ src/poster.js
@@ -0,0 +1,9 @@
+class Poster {
+  constructor(imageURL, title, quote) {
+    this.id = Date.now();
+    this.imageURL = imageURL;
+    this.title = title;
+    this.quote = quote;
+  }
+}
+ export default Poster;
